14b941dd884f9a7c3134b9b90c4b796f2774d026
ParkerKaiMalachai <irinkademchuuuuuuuk@gmail.com>
2025-01-13 12:43:07 +0300

    project init

---
+++ app/Models/User.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Osiset\ShopifyApp\Contracts\ShopModel as IShopModel;
+use Osiset\ShopifyApp\Traits\ShopModel;
+
+final class User extends Authenticatable implements IShopModel
+{
+    use Notifiable;
+    use ShopModel;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+}
